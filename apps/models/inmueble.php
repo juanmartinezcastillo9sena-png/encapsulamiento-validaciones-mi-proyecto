c812743d1c972ec464055f41755d52d80e984e4a
Juan Martinez <juanmartinezcastillo9sena@gmail.com>
2026-08-06 13:43:51 -0500

    Todos los get y set

--- apps/models/inmueble.php
+++ apps/models/inmueble.php
@@ -29,12 +29,100 @@
                 }            
         }
 
+        public function getTipo_inmueble()
+        {
+            return $this->tipo_inmueble;
+        }
+
         public function setDireccion($direccion) 
         {
             if (is_string($direccion) && trim($direccion) !=="")
                 {
                     $this->direccion=$direccion;
                 }            
+        }
+
+         public function getDireccion()
+        {
+            return $this->direccion;
+        }
+
+        public function setArea($area) 
+        {
+            if (is_string($area) && trim($area) !=="")
+                {
+                    $this->area=$area;
+                }            
+        }
+
+         public function getArea()
+        {
+            return $this->area;
+        }
+
+        public function setHabitaciones($habitaciones) 
+        {
+            if (is_numeric($habitaciones) && $habitaciones>=0 && $habitaciones<=20)
+                {
+                    $this->habitaciones=$habitaciones;
+                }            
+        }
+
+         public function getHabitaciones()
+        {
+            return $this->habitaciones;
+        }
+
+        public function setBanos($banos) 
+        {
+            if (is_numeric($banos) && $banos>=0)
+                {
+                    $this->banos=$banos;
+                }            
+        }
+
+         public function getBanos()
+        {
+            return $this->banos;
+        }
+
+        public function setParqueadero($parqueadero) 
+        {
+            if (is_numeric($parqueadero) && $parqueadero>=0)
+                {
+                    $this->parqueadero=$parqueadero;
+                }            
+        }
+
+         public function getParqueadero()
+        {
+            return $this->parqueadero;
+        }
+
+        public function setEstado($estado) 
+        {
+            if (is_string($estado) && trim($estado) !== "" && $estado="Disponible" || $estado="Ocupado" || $estado="Mantenimiento")
+                {
+                    $this->estado=$estado;
+                }            
+        }
+
+         public function getEstado()
+        {
+            return $this->estado;
+        }
+
+        public function setValor_canon($valor_canon)
+        {
+            if (is_numeric($valor_canon) && $valor_canon>=0)
+                {
+                    $this->valor_canon=$valor_canon;
+                }
+        }
+
+         public function getValor_canon()
+        {
+            return $this->valor_canon;
         }
     }
 ?>
